ce721ad9e7163244b4029bea253c00484b25ebde
Syntax-Queen <adebisiqueen231@gmail.com>
2025-10-17 20:33:44 +0100

    initail commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    with app.app_context():
+        db.create_all()
+
+    @app.route('/')
+    def home():
+        return 'Home'
+
+    return app
+
+app = create_app()
